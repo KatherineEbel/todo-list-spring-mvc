3944c2281d276bb734275ea7c5edb05e706248ce
kathyebel <kathyebel@me.com>
2018-06-21 09:58:25 -0400

    Complete lecture 208 'Simple Controller'

---
+++ src/main/java/com/katherineebel/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.katherineebel.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.katherineebel")
+public class WebConfig {
+}
